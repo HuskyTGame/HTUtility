bfd429ed00638e1d4edca63a94f573051af8dc4a
HuskyTGame <18171223217@163.com>
2020-03-25 17:02:16 +0800

    Create Logger.cs

---
+++ Assets/HTUtility/7-Logger/Logger.cs
@@ -0,0 +1,144 @@
+/****************************************************
+	文件：HTLogger.cs
+	作者：HuskyT
+	邮箱：1005240602@qq.com
+	日期：2020/3/25 14:4:3
+	功能：打印消息接口
+*****************************************************/
+//#define LOGGER_OPEN
+using System;
+
+namespace HTUtility
+{
+    /*
+     * 使用方法：
+     * 1.新建 Class 实现 ILoggerListener 接口，实现 Log 方法
+     * 2.初始化 Logger
+     * 3.向 Logger 中不同的 Channel 分别添加 LoggerListener 监听
+     * 4.在菜单栏“HTUtility/6.LoggerSwitch”中开启（切换）Logger 开关
+     */
+    public interface ILoggerListener
+    {
+        void Log(string msg);
+    }
+    public class Logger : HTSingleton<Logger>
+    {
+        public enum Channel
+        {
+            DEFAULT = 0,
+            //打印信息（程序正常运行的信息）
+            INFO,
+            //待完成的信息
+            TODO,
+            //debug时输出的信息
+            DEBUG,
+            //警告信息
+            WARNING,
+            //错误信息
+            ERROR,
+        }
+        private static bool mHasInit = false;
+        //是否需要包装 消息 （给消息加上 日期 和 Channel）
+        private static bool mNeedPackageMsg = false;
+        private static bool[] mChannelEnableArray;
+        private static ILoggerListener[] mChannelListenerArray;
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public void Init(bool needPackageMsg = true)
+        {
+            mChannelEnableArray = new bool[Enum.GetValues(typeof(Channel)).Length];
+            mChannelListenerArray = new ILoggerListener[Enum.GetValues(typeof(Channel)).Length];
+            //默认初始开启所有信息打印通道
+            for (int i = 0; i < mChannelEnableArray.Length; i++)
+            {
+                mChannelEnableArray[i] = true;
+            }
+            mNeedPackageMsg = needPackageMsg;
+            mHasInit = true;
+        }
+        /// <summary>
+        /// 重置
+        /// </summary>
+        public void Reset()
+        {
+            if (mHasInit == false) return;
+            for (int i = 0; i < mChannelListenerArray.Length; i++)
+            {
+                RemoveListener((Channel)i);
+            }
+            mChannelListenerArray = null;
+            mChannelEnableArray = null;
+            mNeedPackageMsg = false;
+            mHasInit = false;
+        }
+        public void AddListener(Channel channel, ILoggerListener listener)
+        {
+            if (mHasInit == false) return;
+            if (mChannelEnableArray[(int)channel] == false) return;
+            mChannelListenerArray[(int)channel] = listener;
+        }
+        public void RemoveListener(Channel channel)
+        {
+            if (mHasInit == false) return;
+            mChannelListenerArray[(int)channel] = null;
+        }
+
+        #region 打印消息
+        public static void Info(string msg)
+        {
+#if LOGGER_OPEN
+            LogMsg(msg, Channel.INFO, mNeedPackageMsg);
+#endif
+        }
+        public static void Todo(string msg)
+        {
+#if LOGGER_OPEN
+            LogMsg(msg, Channel.TODO, mNeedPackageMsg);
+#endif
+        }
+        public static void Debug(string msg)
+        {
+#if LOGGER_OPEN
+            LogMsg(msg, Channel.DEBUG, mNeedPackageMsg);
+#endif
+        }
+        public static void Warning(string msg)
+        {
+#if LOGGER_OPEN
+            LogMsg(msg, Channel.WARNING, mNeedPackageMsg);
+#endif
+        }
+        public static void Error(string msg)
+        {
+#if LOGGER_OPEN
+            LogMsg(msg, Channel.ERROR, mNeedPackageMsg);
+#endif
+        }
+        public static void LogMsg(string msg, Channel channel, bool needPackageMsg)
+        {
+            if (mHasInit == false) return;
+            if (mChannelEnableArray[(int)channel] == false) return;
+            ILoggerListener logger = mChannelListenerArray[(int)channel];
+            if (logger == null) return;
+            string message = msg;
+            if (needPackageMsg)
+                message = PackageMsg(msg, channel);
+            logger.Log(message);
+            Console.WriteLine(message);
+        }
+        #endregion
+
+        /// <summary>
+        /// 包装信息
+        /// </summary>
+        /// <param 信息="msg"></param>
+        /// <param 频道="channel"></param>
+        /// <returns></returns>
+        private static string PackageMsg(string msg, Channel channel)
+        {
+            return string.Format("({0})[{1}]：{2}", DateTime.Now.ToString(), channel, msg);
+        }
+    }
+}
